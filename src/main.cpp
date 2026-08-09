72de35b5627846d4040b0b457c994bb96b319558
alexedwa <alexedwa123@gmail.com>
2026-08-09 17:59:25 +0100

    add correctness check routine

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,12 @@
 #include "main.hpp"
 
 void initialise(double* x, double* w, int size) {
-    std::cout << "Initialising arrays..." << std::endl;
+    std::cout << "\nInitialising arrays..." << std::endl;
 
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            x[i * size + j] = (double) (i%7+0.01);
-            w[i * size + j] = (double) (j%7+0.01);
+            x[i * size + j] = (double)(i%7+0.01);
+            w[i * size + j] = (double)(j%7+0.01);
         }
     }
 
@@ -19,19 +19,37 @@ void initialise(double* x, double* w, int size) {
 
 }
 
+void correctness_check(double* xout, double* xout_check, int size) {
+    std::cout << "\nPerforming correctness check..." << std::endl;
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (std::abs(xout[i * size + j] - xout_check[i * size + j]) > 1e-6) {
+                std::cerr << "Error: Incorrect value at (" << i << ", " << j << "): "<< xout[i * size + j] << " != " << xout_check[i * size + j] << std::endl;
+                return;
+            }
+        }
+    }
+
+    std::cout << "Correctness check passed.\n" << std::endl;
+}
+
 void gemm(int registers, int* cache_line[], int array_size, int reruns) {
     // 2d matrices
     double* x = (double*)malloc(sizeof(double) * array_size * array_size);
     double* w = (double*)malloc(sizeof(double) * array_size * array_size);
     double* xout = (double*)malloc(sizeof(double) * array_size * array_size);
+    double* xout_check = (double*)malloc(sizeof(double) * array_size * array_size);
 
     initialise(x, w, array_size);
+    matmul_base(x, w, xout_check, array_size, registers, reruns);
 
-    base(x, w, xout, array_size, registers, reruns);
+    correctness_check(xout, xout_check, array_size);
 
     free(x);
     free(w);
     free(xout);
+    free(xout_check);
 }
 
 
--- src/main.hpp
+++ src/main.hpp
@@ -1,7 +1,15 @@
 #ifndef MAIN
 #define MAIN
 
+#include <iostream>
+#include <cstring>
+#include <cstdint>
+#include <omp.h>
+
+#include <emmintrin.h> // sse
+#include <immintrin.h> // avx2
+
 // matmul implementations
-void base(double* x, double* w, double* xout, int array_size, int registers, int reruns);
+void matmul_base(double* x, double* w, double* xout, int array_size, int registers, int reruns);
 
 #endif
