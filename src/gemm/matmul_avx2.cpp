50257771fb82981ea5818ed476fb6bc7313182bb
alexedwa <alexedwa123@gmail.com>
2026-08-18 13:10:59 +0100

    Added OMP implementations

--- src/gemm/matmul_avx2.cpp
+++ src/gemm/matmul_avx2.cpp
@@ -1,6 +1,9 @@
 #include <immintrin.h> // avx2
 #include "../main.hpp"
 
+/*
+    AVX2 INTRINSICS IMPLEMENTATIONS
+*/
 void avx2_simd(double* x, double* w, double* xout, int array_size){
     __m256d v_x, v_w, v_xout; // 3 (6 registers used)
     
@@ -27,6 +30,9 @@ void avx2_simd(double* x, double* w, double* xout, int array_size){
 
 
 
+/*
+    REGISTER BLOCKING IMPLEMENTATIONS
+*/
 void avx2_rb4(double* x, double* w, double* xout, int array_size){
     __m256d v_x, v_x2, v_x3, v_x4, v_w, v_xout; // 6 (12 * 2 registers used)
     
@@ -156,7 +162,7 @@ void avx2_rb8(double* x, double* w, double* xout, int array_size){
 }
 
 void avx2_rb12(double* x, double* w, double* xout, int array_size){
-    __m256d v_x, v_x2, v_x3, v_x4, v_x5, v_x6, v_x7, v_x8, v_x9, v_x10, v_x11, v_x12, v_w, v_xout; // 14 (28 * 2 registers used)
+    __m256d v_x, v_x2, v_x3, v_x4, v_x5, v_x6, v_x7, v_x8, v_x9, v_x10, v_x11, v_x12, v_w, v_xout; // 14 (28 registers used)
     
     memset(xout, 0, (size_t)array_size * array_size * sizeof(double));
     
@@ -274,7 +280,10 @@ void avx2_rb16(double* x, double* w, double* xout, int array_size){
 
 
 
-void avx2_rb12_lt(double* x, double* w, double* xout, int array_size, int cache_line[4]){
+/*
+    LOOP TILING IMPLEMENTATIONS
+*/
+void avx2_rb12_lt_l1d(double* x, double* w, double* xout, int array_size, int cache_line[4]){
     __m256d v_x, v_x2, v_x3, v_x4, v_x5, v_x6, v_x7, v_x8, v_x9, v_x10, v_x11, v_x12, v_w, v_xout; // 14 (28 * 2 registers used)
     int l1d_tile = cache_line[1] / (sizeof(double) * 4);
 
@@ -341,6 +350,80 @@ void avx2_rb12_lt(double* x, double* w, double* xout, int array_size, int cache_
 }
 
 
+
+/*
+    OMP IMPLEMENTATIONS
+*/
+void avx2_rb12_lt_l1d_omp(double* x, double* w, double* xout, int array_size, int cache_line[4]){
+    #pragma omp parallel
+    {
+    __m256d v_x, v_x2, v_x3, v_x4, v_x5, v_x6, v_x7, v_x8, v_x9, v_x10, v_x11, v_x12, v_w, v_xout; // 14 (28 * 2 registers used)
+    int l1d_tile = cache_line[1] / (sizeof(double) * 4);
+
+    memset(xout, 0, (size_t)array_size * array_size * sizeof(double));
+    
+    #pragma omp for private(v_x, v_x2, v_x3, v_x4, v_x5, v_x6, v_x7, v_x8, v_x9, v_x10, v_x11, v_x12, v_w, v_xout) nowait schedule(dynamic)
+    for(int kk = 0; kk < array_size; kk += l1d_tile){
+        for(int jj = 0; jj < array_size; jj += l1d_tile){
+            for(int ii = 0; ii < array_size; ii += l1d_tile){
+
+                for(int i = ii; i < std::min(ii + l1d_tile, array_size); ++i){
+                    int k = kk;
+                    for(; k + 11 < std::min(kk + l1d_tile, array_size); k += 12){
+                        v_x = _mm256_set1_pd(x[i * array_size + k]);
+                        v_x2 = _mm256_set1_pd(x[i * array_size + (k + 1)]);
+                        v_x3 = _mm256_set1_pd(x[i * array_size + (k + 2)]);
+                        v_x4 = _mm256_set1_pd(x[i * array_size + (k + 3)]);
+                        v_x5 = _mm256_set1_pd(x[i * array_size + (k + 4)]);
+                        v_x6 = _mm256_set1_pd(x[i * array_size + (k + 5)]);
+                        v_x7 = _mm256_set1_pd(x[i * array_size + (k + 6)]);
+                        v_x8 = _mm256_set1_pd(x[i * array_size + (k + 7)]);
+                        v_x9 = _mm256_set1_pd(x[i * array_size + (k + 8)]);
+                        v_x10 = _mm256_set1_pd(x[i * array_size + (k + 9)]);
+                        v_x11 = _mm256_set1_pd(x[i * array_size + (k + 10)]);
+                        v_x12 = _mm256_set1_pd(x[i * array_size + (k + 11)]);
+
+
+                        int j = jj;
+                        for(; j + 3 < std::min(jj + l1d_tile, array_size); j += 4){
+                            v_w = _mm256_loadu_pd(&w[k * array_size + j]);
+                            v_xout = _mm256_loadu_pd(&xout[i * array_size + j]);
+
+                            v_xout = _mm256_fmadd_pd(v_x, v_w, v_xout);
+                            v_xout = _mm256_fmadd_pd(v_x2, v_w, v_xout);
+                            v_xout = _mm256_fmadd_pd(v_x3, v_w, v_xout);
+                            v_xout = _mm256_fmadd_pd(v_x4, v_w, v_xout);
+                            v_xout = _mm256_fmadd_pd(v_x5, v_w, v_xout);
+                            v_xout = _mm256_fmadd_pd(v_x6, v_w, v_xout);
+                            v_xout = _mm256_fmadd_pd(v_x7, v_w, v_xout);
+                            v_xout = _mm256_fmadd_pd(v_x8, v_w, v_xout);
+                            v_xout = _mm256_fmadd_pd(v_x9, v_w, v_xout);
+                            v_xout = _mm256_fmadd_pd(v_x10, v_w, v_xout);
+                            v_xout = _mm256_fmadd_pd(v_x11, v_w, v_xout);
+                            v_xout = _mm256_fmadd_pd(v_x12, v_w, v_xout);
+
+                            _mm256_storeu_pd(&xout[i * array_size + j], v_xout);
+                        }
+
+                        // cleanup
+                        for (; j < std::min(jj + l1d_tile, array_size); ++j) {
+                            xout[i * array_size + j] += x[i * array_size + k] * w[k * array_size + j];
+                        }
+                    }
+
+                    // cleanup
+                    for(; k < std::min(kk + l1d_tile, array_size); ++k){
+                        for(int j = jj; j < std::min(jj + l1d_tile, array_size); ++j){
+                            xout[i * array_size + j] += x[i * array_size + k] * w[k * array_size + j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+    }
+}
+
 void matmul_avx2(double* x, double* w, double* xout, int array_size, int registers, int cache_line[4], int reruns){
     double start, end;
 
@@ -354,7 +437,10 @@ void matmul_avx2(double* x, double* w, double* xout, int array_size, int registe
         //avx2_rb12(x, w, xout, array_size); // most performant one
         //avx2_rb16(x, w, xout, array_size);
 
-        avx2_rb12_lt(x, w, xout, array_size, cache_line); // t - 1024 most performant
+        //avx2_rb12_lt_l1(x, w, xout, array_size, cache_line); // t - 1024 most performant
+        //avx2_rb12_lt_full(x, w, xout, array_size, cache_line);
+
+        avx2_rb12_lt_l1_omp(x, w, xout, array_size, cache_line);
     }
     end = omp_get_wtime();
 
