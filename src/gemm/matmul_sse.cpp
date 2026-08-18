ad5e442a6fc6d8e67765e401c4a22bcf663af077
alexedwa <alexedwa123@gmail.com>
2026-08-18 13:10:47 +0100

    Added more register blocking and loop tiling implementations

--- src/gemm/matmul_sse.cpp
+++ src/gemm/matmul_sse.cpp
@@ -1,95 +1,324 @@
 #include <emmintrin.h> // sse
 #include "../main.hpp"
 
-void sse_rb2(double* x, double* w, double* xout, int array_size){
-    __m128d v_sum, v_x, v_x2, v_w, v_w2;
+void sse_simd(double* x, double* w, double* xout, int array_size){
+    __m128d v_x, v_w, v_xout; // 3 
+    
+    memset(xout, 0, (size_t)array_size * array_size * sizeof(double));
 
     for(int i = 0; i < array_size; ++i){
+        for(int k = 0; k < array_size; ++k){
+            v_x = _mm_set1_pd(x[i * array_size + k]);
+            int j = 0;
+            for(; j + 1 < array_size; j += 2){
+                v_w = _mm_loadu_pd(&w[k * array_size + j]);
+                v_xout = _mm_loadu_pd(&xout[i * array_size + j]);
 
-        int j = 0;
-        for(; j < array_size; j += 2){ // sse incrementation
-            v_sum = _mm_setzero_pd();
-
-            int k = 0;
-            for(; k < array_size; k += 2){ // rb incrementation
-                v_x  = _mm_set1_pd(x[i * array_size + k]);
-                v_x2 = _mm_set1_pd(x[i * array_size + (k + 1)]);
-                
-                v_w  = _mm_loadu_pd(&w[k * array_size + j]);
-                v_w2 = _mm_loadu_pd(&w[(k + 1) * array_size + j]);
-
-                v_sum = _mm_add_pd(v_sum, _mm_mul_pd(v_x,  v_w));
-                v_sum = _mm_add_pd(v_sum, _mm_mul_pd(v_x2, v_w2));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x, v_w));
+                _mm_storeu_pd(&xout[i * array_size + j], v_xout);
             }
 
-            // cleanup
-            double tail0 = 0.0, tail1 = 0.0;
-            for(; k < array_size; ++k){
-                tail0 += x[i * array_size + k] * w[k * array_size + j];
-                tail1 += x[i * array_size + k] * w[k * array_size + (j + 1)];
+            for (; j < array_size; ++j) {
+                xout[i * array_size + j] += x[i * array_size + k] * w[k * array_size + j];
             }
-
-            double packed[2];
-            _mm_storeu_pd(packed, v_sum);
-            xout[i * array_size + j] = packed[0] + tail0;
-            xout[i * array_size + (j + 1)] = packed[1] + tail1;
-        }
-
-        // cleanup
-        for(; j < array_size; ++j){
-            double res = 0.0;
-            for(int k = 0; k < array_size; ++k){
-                res += x[i * array_size + k] * w[k * array_size + j];
-            }
-            xout[i * array_size + j] = res;
         }
     }
 }
 
-void sse_rb2_lt(double* x, double* w, double* xout, int array_size, int cache_size[4]){
-    __m128d v_sum, v_x, v_x2, v_w, v_w2;
-    int l1d_tile = cache_size[1] / (sizeof(double));
+void sse_rb2(double* x, double* w, double* xout, int array_size){
+    __m128d v_x, v_x2, v_w, v_xout; // 6
+    
+    memset(xout, 0, (size_t)array_size * array_size * sizeof(double));
+
+    for(int i = 0; i < array_size; ++i){
+
+        int k = 0;
+        for(; k + 1 < array_size; k += 2){
+            v_x = _mm_set1_pd(x[i * array_size + k]);
+            v_x2 = _mm_set1_pd(x[i * array_size + (k + 1)]);
+
+            int j = 0;
+            for(; j + 1 < array_size; j += 2){
+                v_w = _mm_loadu_pd(&w[k * array_size + j]);
+                v_xout = _mm_loadu_pd(&xout[i * array_size + j]);
+
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x2, v_w));
+
+                _mm_storeu_pd(&xout[i * array_size + j], v_xout);
+            }
+
+            // cleanup
+            for (; j < array_size; ++j) {
+                xout[i * array_size + j] += x[i * array_size + k] * w[k * array_size + j];
+            }
+        }
+
+        // cleanup
+        for(; k < array_size; ++k){
+            for(int j = 0; j < array_size; ++j){
+                xout[i * array_size + j] += x[i * array_size + k] * w[k * array_size + j];
+            }
+        }
+    }
+}
+
+void sse_rb4(double* x, double* w, double* xout, int array_size){
+    __m128d v_x, v_x2, v_x3, v_x4, v_w, v_xout; // 6
+    
+    memset(xout, 0, (size_t)array_size * array_size * sizeof(double));
+
+    for(int i = 0; i < array_size; ++i){
+
+        int k = 0;
+        for(; k + 3 < array_size; k += 4){
+            v_x = _mm_set1_pd(x[i * array_size + k]);
+            v_x2 = _mm_set1_pd(x[i * array_size + (k + 1)]);
+            v_x3 = _mm_set1_pd(x[i * array_size + (k + 2)]);
+            v_x4 = _mm_set1_pd(x[i * array_size + (k + 3)]);
+            int j = 0;
+            for(; j + 1 < array_size; j += 2){
+                v_w = _mm_loadu_pd(&w[k * array_size + j]);
+                v_xout = _mm_loadu_pd(&xout[i * array_size + j]);
+
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x2, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x3, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x4, v_w));
+
+                _mm_storeu_pd(&xout[i * array_size + j], v_xout);
+            }
+
+            // cleanup
+            for (; j < array_size; ++j) {
+                xout[i * array_size + j] += x[i * array_size + k] * w[k * array_size + j];
+            }
+        }
+
+        // cleanup
+        for(; k < array_size; ++k){
+            for(int j = 0; j < array_size; ++j){
+                xout[i * array_size + j] += x[i * array_size + k] * w[k * array_size + j];
+            }
+        }
+    }
+}
+
+void sse_rb8(double* x, double* w, double* xout, int array_size){
+    __m128d v_x, v_x2, v_x3, v_x4, v_x5, v_x6, v_x7, v_x8, v_w, v_xout; // 10
+    
+    memset(xout, 0, (size_t)array_size * array_size * sizeof(double));
+
+    for(int i = 0; i < array_size; ++i){
+
+        int k = 0;
+        for(; k + 7 < array_size; k += 8){
+            v_x = _mm_set1_pd(x[i * array_size + k]);
+            v_x2 = _mm_set1_pd(x[i * array_size + (k + 1)]);
+            v_x3 = _mm_set1_pd(x[i * array_size + (k + 2)]);
+            v_x4 = _mm_set1_pd(x[i * array_size + (k + 3)]);
+            v_x5 = _mm_set1_pd(x[i * array_size + (k + 4)]);
+            v_x6 = _mm_set1_pd(x[i * array_size + (k + 5)]);
+            v_x7 = _mm_set1_pd(x[i * array_size + (k + 6)]);
+            v_x8 = _mm_set1_pd(x[i * array_size + (k + 7)]);
+
+            int j = 0;
+            for(; j + 1 < array_size; j += 2){
+                v_w = _mm_loadu_pd(&w[k * array_size + j]);
+                v_xout = _mm_loadu_pd(&xout[i * array_size + j]);
+
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x2, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x3, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x4, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x5, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x6, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x7, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x8, v_w));
+
+                _mm_storeu_pd(&xout[i * array_size + j], v_xout);
+            }
+
+            // cleanup
+            for (; j < array_size; ++j) {
+                xout[i * array_size + j] += x[i * array_size + k] * w[k * array_size + j];
+            }
+        }
+
+        // cleanup
+        for(; k < array_size; ++k){
+            for(int j = 0; j < array_size; ++j){
+                xout[i * array_size + j] += x[i * array_size + k] * w[k * array_size + j];
+            }
+        }
+    }
+}
+
+void sse_rb10(double* x, double* w, double* xout, int array_size){
+    __m128d v_x, v_x2, v_x3, v_x4, v_x5, v_x6, v_x7, v_x8, v_x9, v_x10, v_w, v_xout; // 12
+    
+    memset(xout, 0, (size_t)array_size * array_size * sizeof(double));
+
+    for(int i = 0; i < array_size; ++i){
+
+        int k = 0;
+        for(; k + 9 < array_size; k += 10){
+            v_x = _mm_set1_pd(x[i * array_size + k]);
+            v_x2 = _mm_set1_pd(x[i * array_size + (k + 1)]);
+            v_x3 = _mm_set1_pd(x[i * array_size + (k + 2)]);
+            v_x4 = _mm_set1_pd(x[i * array_size + (k + 3)]);
+            v_x5 = _mm_set1_pd(x[i * array_size + (k + 4)]);
+            v_x6 = _mm_set1_pd(x[i * array_size + (k + 5)]);
+            v_x7 = _mm_set1_pd(x[i * array_size + (k + 6)]);
+            v_x8 = _mm_set1_pd(x[i * array_size + (k + 7)]);
+            v_x9 = _mm_set1_pd(x[i * array_size + (k + 8)]);
+            v_x10 = _mm_set1_pd(x[i * array_size + (k + 9)]);
+
+
+            int j = 0;
+            for(; j + 1 < array_size; j += 2){
+                v_w = _mm_loadu_pd(&w[k * array_size + j]);
+                v_xout = _mm_loadu_pd(&xout[i * array_size + j]);
+
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x2, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x3, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x4, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x5, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x6, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x7, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x8, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x9, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x10, v_w));
+
+
+                _mm_storeu_pd(&xout[i * array_size + j], v_xout);
+            }
+
+            // cleanup
+            for (; j < array_size; ++j) {
+                xout[i * array_size + j] += x[i * array_size + k] * w[k * array_size + j];
+            }
+        }
+
+        // cleanup
+        for(; k < array_size; ++k){
+            for(int j = 0; j < array_size; ++j){
+                xout[i * array_size + j] += x[i * array_size + k] * w[k * array_size + j];
+            }
+        }
+    }
+}
+
+void sse_rb12(double* x, double* w, double* xout, int array_size){
+    __m128d v_x, v_x2, v_x3, v_x4, v_x5, v_x6, v_x7, v_x8, v_x9, v_x10, v_x11, v_x12, v_w, v_xout; // 14
+    
+    memset(xout, 0, (size_t)array_size * array_size * sizeof(double));
+
+    for(int i = 0; i < array_size; ++i){
+        int k = 0;
+        for(; k + 11 < array_size; k += 12){
+            v_x = _mm_set1_pd(x[i * array_size + k]);
+            v_x2 = _mm_set1_pd(x[i * array_size + (k + 1)]);
+            v_x3 = _mm_set1_pd(x[i * array_size + (k + 2)]);
+            v_x4 = _mm_set1_pd(x[i * array_size + (k + 3)]);
+            v_x5 = _mm_set1_pd(x[i * array_size + (k + 4)]);
+            v_x6 = _mm_set1_pd(x[i * array_size + (k + 5)]);
+            v_x7 = _mm_set1_pd(x[i * array_size + (k + 6)]);
+            v_x8 = _mm_set1_pd(x[i * array_size + (k + 7)]);
+            v_x9 = _mm_set1_pd(x[i * array_size + (k + 8)]);
+            v_x10 = _mm_set1_pd(x[i * array_size + (k + 9)]);
+            v_x11 = _mm_set1_pd(x[i * array_size + (k + 10)]);
+            v_x12 = _mm_set1_pd(x[i * array_size + (k + 11)]);
+
+            int j = 0;
+            for(; j + 1 < array_size; j += 2){
+                v_w = _mm_loadu_pd(&w[k * array_size + j]);
+                v_xout = _mm_loadu_pd(&xout[i * array_size + j]);
+
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x2, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x3, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x4, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x5, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x6, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x7, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x8, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x9, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x10, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x11, v_w));
+                v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x12, v_w));
+
+                _mm_storeu_pd(&xout[i * array_size + j], v_xout);
+            }
+
+            // cleanup
+            for (; j < array_size; ++j) {
+                xout[i * array_size + j] += x[i * array_size + k] * w[k * array_size + j];
+            }
+        }
+
+        // cleanuip
+        for(; k < array_size; ++k){
+            for(int j = 0; j < array_size; ++j){
+                xout[i * array_size + j] += x[i * array_size + k] * w[k * array_size + j];
+            }
+        }
+    }
+}
+
+
+
+
+void sse_rb8_lt_l1d(double* x, double* w, double* xout, int array_size, int cache_line[4]){
+    __m128d v_x, v_x2, v_x3, v_x4, v_x5, v_x6, v_x7, v_x8, v_w, v_xout; // 10
+    int l1d_tile = cache_line[1] / (sizeof(double) * 8);
+    memset(xout, 0, (size_t)array_size * array_size * sizeof(double));
 
     for(int kk = 0; kk < array_size; kk += l1d_tile){
         for(int jj = 0; jj < array_size; jj += l1d_tile){
             for(int ii = 0; ii < array_size; ii += l1d_tile){
-                for(int i = 0; i < array_size; ++i){
-                    int j = jj;
-                    for(; j + 1< array_size; j += 2){ // sse incrementation
-                        v_sum = _mm_setzero_pd();
 
-                        int k = kk;
-                        for(; k + 1< array_size; k += 2){ // rb incrementation
-                            v_x  = _mm_set1_pd(x[i * array_size + k]);
-                            v_x2 = _mm_set1_pd(x[i * array_size + (k + 1)]);
-                            
-                            v_w  = _mm_loadu_pd(&w[k * array_size + j]);
-                            v_w2 = _mm_loadu_pd(&w[(k + 1) * array_size + j]);
+                for(int i = ii; i < std::min(ii + l1d_tile, array_size); ++i){
+                    int k = kk;
+                    for(; k + 7 < std::min(kk + l1d_tile, array_size); k += 8){
+                        v_x = _mm_set1_pd(x[i * array_size + k]);
+                        v_x2 = _mm_set1_pd(x[i * array_size + (k + 1)]);
+                        v_x3 = _mm_set1_pd(x[i * array_size + (k + 2)]);
+                        v_x4 = _mm_set1_pd(x[i * array_size + (k + 3)]);
+                        v_x5 = _mm_set1_pd(x[i * array_size + (k + 4)]);
+                        v_x6 = _mm_set1_pd(x[i * array_size + (k + 5)]);
+                        v_x7 = _mm_set1_pd(x[i * array_size + (k + 6)]);
+                        v_x8 = _mm_set1_pd(x[i * array_size + (k + 7)]);
 
-                            v_sum = _mm_add_pd(v_sum, _mm_mul_pd(v_x,  v_w));
-                            v_sum = _mm_add_pd(v_sum, _mm_mul_pd(v_x2, v_w2));
+                        int j = jj;
+                        for(; j + 1 < std::min(jj + l1d_tile, array_size); j += 2){
+                            v_w = _mm_loadu_pd(&w[k * array_size + j]);
+                            v_xout = _mm_loadu_pd(&xout[i * array_size + j]);
+
+                            v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x, v_w));
+                            v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x2, v_w));
+                            v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x3, v_w));
+                            v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x4, v_w));
+                            v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x5, v_w));
+                            v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x6, v_w));
+                            v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x7, v_w));
+                            v_xout = _mm_add_pd(v_xout, _mm_mul_pd(v_x8, v_w));
+
+                            _mm_storeu_pd(&xout[i * array_size + j], v_xout);
                         }
 
-                        // cleanup
-                        double tail0 = 0.0, tail1 = 0.0;
-                        for(; k < array_size; ++k){
-                            tail0 += x[i * array_size + k] * w[k * array_size + j];
-                            tail1 += x[i * array_size + k] * w[k * array_size + (j + 1)];
+                        //cleanup
+                        for (; j <  std::min(jj + l1d_tile, array_size); ++j) {
+                            xout[i * array_size + j] += x[i * array_size + k] * w[k * array_size + j];
                         }
-
-                        double packed[2];
-                        _mm_storeu_pd(packed, v_sum);
-                        xout[i * array_size + j] = packed[0] + tail0;
-                        xout[i * array_size + (j + 1)] = packed[1] + tail1;
                     }
 
                     // cleanup
-                    for(; j < array_size; ++j){
-                        double res = 0.0;
-                        for(int k = 0; k < array_size; ++k){
-                            res += x[i * array_size + k] * w[k * array_size + j];
+                    for(; k <  std::min(kk + l1d_tile, array_size); ++k){
+                        for(int j = 0; j < array_size; ++j){
+                            xout[i * array_size + j] += x[i * array_size + k] * w[k * array_size + j];
                         }
-                        xout[i * array_size + j] = res;
                     }
                 }
             }
@@ -102,8 +331,15 @@ void matmul_sse(double* x, double* w, double* xout, int array_size, int register
 
     start = omp_get_wtime();
     for(int i = 0; i < reruns; ++i){
+        //sse_simd(x, w, xout, array_size);
+
         //sse_rb2(x, w, xout, array_size);
-        sse_rb2_lt(x, w, xout, array_size, cache_line);
+        //sse_rb4(x, w, xout, array_size);
+        //sse_rb8(x, w, xout, array_size); // most optimal for 8 registers
+        //sse_rb10(x, w, xout, array_size);
+        //sse_rb12(x, w, xout, array_size);
+
+        sse_rb8_lt_l1d(x, w, xout, array_size, cache_line);
     }
     end = omp_get_wtime();
 
