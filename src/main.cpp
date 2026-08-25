e9c94ef77af9ae282ce737f513b32e5a67d77645
alexedwa <alexedwa123@gmail.com>
2026-08-25 18:51:52 +0100

    changed args for the removal of registers

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,8 @@ void gemm(int array_size, int reruns) {
     initialise(x, w, array_size);
     matmul_unoptimised(x, w, xout_check, array_size, reruns);
 
-    //matmul_base_scalar(x, w, xout, array_size, registers, reruns);
-    //matmul_sse(x, w, xout, array_size, registers, cache_size, reruns);
+    //matmul_base_scalar(x, w, xout, array_size, reruns);
+    //matmul_sse(x, w, xout, array_size, cache_size, reruns);
     matmul_avx2(x, w, xout, array_size, cache_size, reruns);
 
     correctness_check(xout, xout_check, array_size);
